b8a97a215503cabc292de4dfa8a2cf1b749ed51b
Wesley <wesmith314@gmail.com>
2023-09-08 00:14:03 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+ if (feet <= 400) {
+  result = "This one is on me!"
+  } else if (feet > 400 && feet < 2000) {
+  result = "That will be twenty bucks."
+    } else if (feet > 2000 && feet < 2500) {
+  result = "I will gladly take your thirty bucks."
+      } else if (feet >= 2500) {
+  result = "No can do."
+  }
+return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+ if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
+}
 }
